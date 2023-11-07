ad6357ec6c504ead598035a647db44e85a64f596
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2023-11-07 11:45:42 +0530

    Setup JSON-SERVER

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import Header from "./Header"
 import Main from "./Main";
+
 function App() {
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch((err) => console.log(`Error`))
+  }, [])
+
   return <>
     <div className="app">
       <Header />
