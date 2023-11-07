63d37f557f315e55be467ef2cba1951076053043
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2023-11-07 11:36:35 +0530

    Setup Main Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Header from "./Header"
+import Main from "./Main";
 function App() {
-  return <div className="app">
-    <Header />
-  </div>
+  return <>
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
+    </div>
+  </>
 }
 
 export default App;
